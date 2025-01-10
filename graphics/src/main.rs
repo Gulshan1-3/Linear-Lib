84150e6fbda073e5938bfb27165e28cd3f345661
Gulshan1-3 <ankitstar955@gmail.com>
2025-01-10 13:34:48 +0530

    vulkan

--- graphics/src/main.rs
+++ graphics/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused)]
-use anyhow::Result;
+use anyhow::{Result,Context};
 use ash::{self, vk};
 use winit::{
     event_loop::EventLoop,
@@ -8,9 +8,18 @@ use winit::{
 #[allow(unused_variables)]
 fn main() -> Result<()> {
     let entry = unsafe { ash::Entry::load() }?;
-    let application_info = vk::ApplicationInfo::builder().api_version(vk::API_VERSION_1_3);
-    let create_info = vk::InstanceCreateInfo::builder().application_info(&application_info); //The VkInstanceCreateInfo structure is used to specify which validation layers and global extensions to                                                                                  // use when creating a Vulkan instance
-    let instance = unsafe { entry.create_instance(&create_info, None) }?;
+    //The VkInstanceCreateInfo structure is used to specify which validation layers and global extensions to                                                                                  // use when creating a Vulkan instance
+    let instance = {
+        let application_info = vk::ApplicationInfo::builder().api_version(vk::API_VERSION_1_3);
+        let create_info = vk::InstanceCreateInfo::builder().application_info(&application_info);
+        unsafe { entry.create_instance(&create_info, None) }?
+};
+    let physcial_device = unsafe {
+        instance.enumerate_physical_devices()
+    }?.into_iter().next().context("NO Physical device found");
+    let device = unsafe {
+        instance.create_device(physical_device, &create_info, None)
+    }?;
     unsafe { instance.destroy_instance(None) }
     Ok(())
 }
